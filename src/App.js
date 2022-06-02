dfd8edff242206481d9513f83ae6425f9e73e7d8
ackermanaakriti <bhandariaakriti897@gmail.com>
2022-06-02 17:55:21 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import {useState} from 'react'
 
 function App() {
+  const [currVal, SetcurVal]= useState('')
+  const [result, setResult]= useState('');
+
+  const ops=['/','*','+','-','.'];
+  const Createdigit=()=>
+  {
+    const digit=[];
+    for(let i=0;i<10;i++)
+    {
+      
+      digit.push( <button onClick={()=>Handleclick(i)} value={i} key={i}>{i}</button>)
+      
+    }
+    return digit;
+    
+  }
+ 
+ 
+  const Handleclick=(value)=>
+  {
+    
+    SetcurVal(currVal+value);
+    
+  }
+ 
+  const Handleonchange=()=>
+  {
+
+  }
+ const Handleresult=( value)=>
+ {
+   setResult(eval(currVal+value).toString())
+   SetcurVal(...result, currVal);
+
+ }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='input'>
+       <input type='text' onChange={Handleonchange} value={currVal} />
+      
+      </div>
+      <div className='operators'>
+     <button onClick={()=>Handleclick('/')} >/</button>
+     <button onClick={()=>Handleclick('*')} >*</button>
+     <button onClick={()=>Handleclick('-')} >-</button>
+     <button onClick={()=>Handleclick('+')} >+</button>
+     <button>DEL</button>
+      </div>
+      <div className='digits'>
+        {Createdigit()}
+        
+        <button onClick={()=>Handleclick('.')} >.</button>
+        <button onClick={()=>Handleresult('')} >=</button>
+      </div>
     </div>
   );
 }
